8a48e3881d88846d746ff9600135d4eb4d3d1ea0
erfan-hamidi <erfan9757@gmail.com>
2022-01-05 19:24:05 +0330

    complete model and admin setting

--- foods/models.py
+++ foods/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.db.models.fields import CharField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class category(models.Model):
+    name = CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class food(models.Model):
     name = CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField()
+    image = models.ImageField(upload_to="media", null= True)
+    category = ForeignKey(category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
     
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/user-media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
