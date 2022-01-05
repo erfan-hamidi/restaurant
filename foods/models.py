887a028cbd28499152561de59bcfdc0eee8dfc85
erfan-hamidi <erfan9757@gmail.com>
2022-01-05 18:25:11 +0330

    add model class

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class food(models.Model):
+    name = CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    
