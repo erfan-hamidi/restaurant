8c17decc181fe2d0e71d0b95835c3f324d467935
erfan-hamidi <erfan9757@gmail.com>
2022-01-06 10:21:31 +0330

    get data from database and show in index.html

--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def index(req):
-    return render(req, "foods/index.html")
+    foods = models.food.objects.all()[:1]
+    return render(req, "foods/index.html",{
+        "foods": foods
+    })
