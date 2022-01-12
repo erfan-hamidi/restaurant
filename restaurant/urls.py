69f2dbfa4994a63f708566af54ab1af44ec3e9d0
erfan-hamidi <erfan9757@gmail.com>
2022-01-12 21:16:53 +0330

    setup reservation page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("foods.urls"))
+    path("", include("foods.urls")),
+    path("book", include("reservation.urls")),
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
