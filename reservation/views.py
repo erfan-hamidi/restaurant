27c77d83a241381fc71fda6cc3bc301cc0f8e8ee
erfan-hamidi <erfan9757@gmail.com>
2022-01-15 19:00:05 +0330

    setup form in view

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .forms import ReservationForm
 
 # Create your views here.
 
 def index(req):
     #foods = models.food.objects.all()[:1]
+    reveform = ReservationForm()
+    if req.method == "POST":
+        reveform = ReservationForm(req.POST)
+        if reveform.is_valid():
+            reveform.save()
+    else:
+        reveform = ReservationForm()
+
+
     return render(req, "reservation/reservation.html")
