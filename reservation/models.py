0f8f930196317f74c5d205912aa88213a734e038
erfan-hamidi <erfan9757@gmail.com>
2022-01-15 18:32:57 +0330

    add model class reservation

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    number = models.IntegerField()
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+    
